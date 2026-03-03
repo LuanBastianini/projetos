ad04f4b5b6aba0c78ecc2c19443e5b497db1a0db
Luan Felipe <luan@smn.com.br>
2026-03-03 13:17:39 -0300

    Estudo com Ia

---
+++ IALendoPDF/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System;
+using System.Text;
+using UglyToad.PdfPig;
+using System.Threading.Tasks;
+using OpenAI;
+using OpenAI.Responses;
+using System.Text.Json;
+
+var httpClient = new HttpClient
+{
+    Timeout = TimeSpan.FromMinutes(60),
+    BaseAddress = new Uri("https://api.openai.com/v1/"),
+};
+httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {Environment.GetEnvironmentVariable("OPENAI_API_KEY", EnvironmentVariableTarget.User)}");
+
+var body = new    
+{
+    model = "gpt-5-nano",
+    input = "Olá, tudo bem?"
+};
+
+var response = await httpClient.PostAsync("responses", new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json"));
+var responseContent = await response.Content.ReadAsStringAsync();
+Console.WriteLine(responseContent);
+
+// var prompt = File.ReadAllText("ArquivoLeitura/Prompt.txt").Replace("@TEXTO", text).Replace("@PERGUNTA", "quem é a analista do escopo?");
+
+// var pdfFile = "ArquivoLeitura/Escopo.pdf";
+
+
+
+// var response = await client.Responses.CreateAsync(new ResponseCreateRequest
+// {
+//     Model = "gpt-5 nano",
+//     Input = "Say 'this is a test.'"
+// });
+
+// Console.WriteLine($"[ASSISTANT]: {response.OutputText()}");
+
+
+// var httpClient = new HttpClient
+// {
+//     Timeout = TimeSpan.FromMinutes(60),
+//     BaseAddress = new Uri("http://localhost:11434/")
+// };
+// var ollama = new OllamaApiClient(httpClient, "gpt-oss");
+// string text = "";
+// using (var pdf = PdfDocument.Open(pdfFile))
+//     {
+//         foreach (var page in pdf.GetPages())
+//         {
+//             text += page.Text;
+//         }
+//     }
+
+// ConversationContext context = null;
+// await foreach (var stream in ollama.StreamCompletion(prompt, context))
+// {
+//     Console.Write(stream.Response);
+// }
--- PresentesAna/Models/Presentes.cs
+++
@@ -1,7 +0,0 @@
-namespace PresentesAna.Models;
-
-public class Presentes
-{
-    public int ID { get; set; }
-    public string NomeConvidado { get; set; }
-}
