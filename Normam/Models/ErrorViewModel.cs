44380673c6a94679629dedda4476a91bd2cf4267
Luan Felipe <luan@smn.com.br>
2026-03-10 15:14:43 -0300

    Criação do projeto de chat normam

---
+++ Normam/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Normam.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Normam/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$("#btnChat").click(function(){
+    var model = $('form').serializeObject();
+    $("#chatList").append('<li class="uk-text-right">'+ model.inputChat +'</li>')
+});
